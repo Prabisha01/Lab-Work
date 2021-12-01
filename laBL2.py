d65783e35b5a0b92fa5539e4d77b82ed223ecce4
Prabisha01 <prabishakhadka2000@gmail.com>
2021-12-01 12:43:28 +0545

    third commit

--- laBL2.py
+++ laBL2.py
@@ -1,5 +1,5 @@
 l=int(input("enter the height of triangle: "))
 b=int(input("enter the breath of triangle: "))
 
-a= (l*b)/2
-print("area of triangle is:", a)
+a= (l*b)//2
+print("area of triangle is {} cm\u00b2".format(a) )
