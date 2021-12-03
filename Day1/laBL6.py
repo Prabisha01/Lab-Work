43fa5c3a3805f37895d7a297b0d40ba849722d77
Prabisha01 <prabishakhadka2000@gmail.com>
2021-12-03 20:41:36 +0545

    first commit

--- Day1/laBL6.py
+++ Day1/laBL6.py
@@ -2,15 +2,8 @@
 
 #sum of first 10 odd integer
 
-
-num = range (1, 10)
-sum = 0;
-
-for i in range(1, 10):
-
-    if(not (i % 2) == 0):
-        sum += i;
-
-print("Sum of odd numbers is :", sum)
+a =range(1,11,2)
+sumA= sum(a)
+print(sumA)
 
 
--- Day1/laBl7.py
+++ Day1/laBl7.py
@@ -1,11 +1,6 @@
 #sum of first ten even integer
 
-number = range (1, 11)
+a = range(0,11,2)
 
-sum =0,
-
-for i in range (1, 11):
-     if(not ( i % 2) == 0):
-             sum += i;
-
-print("Sum of even numbers is :", sum)
+sumA= sum(a)
+print(sumA)
