27e58d4f890b761a6b2a44fe58f7ed18d2f9d112
Prabisha01 <prabishakhadka2000@gmail.com>
2021-12-03 13:14:27 +0545

    update

---
+++ Day2/lab2.py
@@ -0,0 +1,6 @@
+#BMI of a person
+# BMI = mass in kg/(height in m)2
+mass= float(input ("enter the mass in kg:"))
+height=float(input("enter the height in meter"))
+BMI= (mass/(height*height))
+print(f"the BMI value of a person is {BMI}")
