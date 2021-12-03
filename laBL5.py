c6a385edbca5c7787f7c2c0f1e424bdd03690ec5
Prabisha01 <prabishakhadka2000@gmail.com>
2021-12-03 12:28:42 +0545

    updated file

--- laBL5.py
+++
@@ -1,5 +0,0 @@
-PI=3.14
-r= int(input("enter the radius of circle: "))
-a = PI*r*r
-
-print("area of circle is {}cm\u00b2". format(a))
--- laBl4.py
+++
@@ -1,6 +0,0 @@
-#sum of first ten integer
-
-listNum=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-sum = sum(listNum)
-print(sum)
-
