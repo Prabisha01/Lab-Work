d12b4e24e6a5ecc51dadd7d4af05537eb10d4a89
Prabisha01 <prabishakhadka2000@gmail.com>
2021-12-03 13:31:00 +0545

    update

---
+++ Day2/lab3.py
@@ -0,0 +1,8 @@
+n= int(input("enter the minutes passed since midnight:"))
+
+hours= n // 60
+minutes=n % 60
+
+print(f" the hours is {hours}")
+print(f" the minutes is {minutes}")
+print(f"(hours):(minutes)")
