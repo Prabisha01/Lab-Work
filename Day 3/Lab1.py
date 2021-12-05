3559ec68f8d5d1922bfd77f57d265c7f14aa7434
Prabisha01 <prabishakhadka2000@gmail.com>
2021-12-05 14:01:34 +0545

    first commit

---
+++ Day 3/Lab1.py
@@ -0,0 +1,8 @@
+#Check whether 5 is in list of first 5 natural numbers or not.
+
+number =int(input("enter the number") )
+if number==5:
+    print("number is natural number")
+
+else:
+    print("number is not natural number")
---
+++ Day 3/lab2.py
@@ -0,0 +1,9 @@
+marks= int(input("enter the marks:"))
+if marks> 75 and marks<=100:
+    print ("congrats you got Distinction")
+elif marks> 60 and marks<=75:
+    print ("congratulation you got first division" )
+elif marks>40 and marks<=60:
+    print ("congratulation you are pass")
+else:
+    print("you are fail")
