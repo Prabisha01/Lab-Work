43d2f928b53e944d4d3bdd39c1a9afad085abd6d
Prabisha01 <prabishakhadka2000@gmail.com>
2021-12-05 14:16:47 +0545

    updated

---
+++ Day 3/Lab4.py
@@ -0,0 +1,10 @@
+a = int (input ("enter the first number"))
+b = int (input ("enter the second number"))
+c= int (input ("enter the third number"))
+
+if a<b and a<c:
+    print (a)
+if b<c and b<c:
+    print (b)
+if c<a and c<a:
+    print (c)
---
+++ Day 3/lab5.py
@@ -0,0 +1,8 @@
+x= int(input("enter the number"))
+
+if x < 0:
+    print ("True")
+elif x>0:
+    print ("False")
+else:
+    print ("zero")
