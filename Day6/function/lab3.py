79c8654772edff9ca3c7e8a074e9e8fa587f9dca
Prabisha01 <prabishakhadka2000@gmail.com>
2021-12-22 08:15:38 +0545

    updated

---
+++ Day6/function/lab3.py
@@ -0,0 +1,3 @@
+# Write a function called showNumbers that takes a parameter called limit. It 
+#should print all the numbers between 0 and limit with a label to identify the even and 
+#odd numbers. 
--- Day6/list/lab4.py
+++ Day6/list/lab4.py
@@ -1,3 +1,3 @@
-#python program to get the smallest number from a list
-ist = [1, 2, 3, 4, 5, 6]
-print("The smallest number from list is: " , min(list))
+#python program to find the smallest number from the list
+list = [1, 2, 3, 4, 5, 6]
+print("The smallest  from list is: " , min(list))
